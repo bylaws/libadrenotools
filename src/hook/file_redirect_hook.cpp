0e4dccf696eac44fceea2e10e7f1bd479e5b8301
Billy Laws <blaws05@gmail.com>
2022-12-27 20:06:50 +0000

    Remove file redir hook remenants

--- src/hook/file_redirect_hook.cpp
+++
@@ -1,9 +0,0 @@
-#include "hook_impl.h"
-
-__attribute__((visibility("default"))) void init_hook_param(const void *param) {
-	init_file_redirect_hook_param(param);
-}
-
-__attribute__((visibility("default"))) FILE *fopen(const char *filename, const char *mode) {
-	return hook_fopen(filename, mode);
-}
